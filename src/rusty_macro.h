3c420942f9d2ec5585e9a9951de1feab6ad33c4f
myyrakle <sssang97@naver.com>
2020-12-19 20:31:45 +0900

    Create rusty_macro.h

---
+++ src/rusty_macro.h
@@ -0,0 +1,17 @@
+#define let const auto
+#define let_mut auto
+#define fn auto
+#define loop while(1)
+
+#define i8 signed char
+#define i16 signed short
+#define i32 signed int
+#define i64 signed long long
+
+#define u8 unsigned char
+#define u16 unsigned short
+#define u32 unsigned int
+#define u64 unsigned long long
+
+#define f32 float
+#define f64 double
